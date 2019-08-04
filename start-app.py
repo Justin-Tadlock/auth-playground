d6e603a4d8da329ce7e7ab0855f9323bada28984
JustinTadlock <justin.b.tadlock@gmail.com>
2019-08-03 19:41:37 -0700

    Cleaning up code
    Adding comments to clarify logic
    Moved logic around to be better sorted
    Formatted based on pyStyleGuide

--- start-app.py
+++ start-app.py
@@ -3,11 +3,11 @@ import requests
 import httplib2
 
 from flask import (
-    Flask, 
+    Flask,
     jsonify,
     session as login_session,
-    redirect, 
-    url_for, 
+    redirect,
+    url_for,
     request,
     make_response,
     render_template
@@ -15,6 +15,7 @@ from flask import (
 
 from google.oauth2 import id_token
 from google.auth.transport import requests
+
 
 # Set up the application
 app = Flask(__name__)
@@ -36,45 +37,81 @@ try:
 except:
     print('Error: Please download your \'client_secrets.json\' file from your \'https://console.developers.google.com\' project')
 
+# Create data used to print into the user posts table
+user_posts = [
+    {"name": "Billy", "secret": "Likes Polka dot patterns, but will never admit it."},
+    {"name": "Bobby", "secret": "Thinks Billy needs a shower."},
+    {"name": "Lauren", "secret": "Wants John to ask her out"},
+    {"name": "John", "secret": "Wants to ask Lauren out, but she's always so cold to him. He's not sure if he should ask her or not."}
+]
+
 
 def Is_Authenticated():
     logged_in = False
+
     if 'user' in login_session:
         logged_in = True
-    
+
     return logged_in
+
 
 @app.route(CLIENT_REDIRECT, methods=['POST'])
 def Authentication_Callback():
     try:
+        # Check if the POST request is trying to log in
         if 'idtoken' in request.form:
             if not Is_Authenticated():
+                # Get the token from the POST form
                 token = request.form['idtoken']
-            
-                # Specify the CLIENT_ID of the app that accesses the backend:
-                idinfo = id_token.verify_oauth2_token(token, requests.Request(), CLIENT_ID)
 
-                if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
+                # Specify the CLIENT_ID of the app that accesses the backend:
+                idinfo = id_token.verify_oauth2_token(
+                    token,
+                    requests.Request(),
+                    CLIENT_ID
+                )
+
+                verified_providers = [
+                    'accounts.google.com',
+                    'https://accounts.google.com'
+                ]
+
+                if idinfo['iss'] not in verified_providers:
                     raise ValueError('Wrong issuer.')
 
-                # ID token is valid. Get the user's Google Account ID from the decoded token.
+                # ID token is valid.
+                # Get the user's Google Account ID from the decoded token.
                 userid = idinfo['sub']
+
+                # Add the token to the flask session variable
                 login_session['user'] = token
+
                 ret_response = make_response(
-                    jsonify(message='Successfully verified token id. You are logged in!', status=200)
+                    jsonify(
+                        message='Successfully verified. You are logged in!',
+                        status=200)
                 )
+
+            # If the user is already logged in,
+            # we don't need to do any authentication.
             else:
                 ret_response = make_response(
                     jsonify(message='User is already logged in.', status=201)
                 )
+
+        # If the POST request does not contain the idtoken field,
+        # that means we are trying to log out.
         else:
-            if 'user' in login_session:
-                print('Logged user with token \'%s\' out.' % login_session['user'])
+            # When we have a logged in user,
+            # we should remove their token from our
+            # logged in sessions variable
+            if Is_Authenticated():
                 login_session.pop('user', None)
+
             ret_response = make_response(
                 jsonify(message="User has been logged out", status=200)
             )
-            
+
     except ValueError:
         # Invalid token
         ret_response = make_response(
@@ -83,24 +120,20 @@ def Authentication_Callback():
 
     return ret_response
 
+
 @app.route('/')
 def Index():
-    user_posts = [
-        {"name":"Billy", "secret":"Likes Polka dot patterns, but will never admit it."},
-        {"name":"Bobby", "secret":"Thinks Billy needs a shower."},
-        {"name":"Lauren", "secret":"Wants John to ask her out"},
-        {"name":"John", "secret":"Wants to ask Lauren out, but she's always so cold to him. He's not sure if he should ask her or not."}
-    ]
-
-    print('authenticated: %s' % Is_Authenticated())
-
-    return render_template('index.html', client_id=CLIENT_ID, authenticated=Is_Authenticated(), user_posts=user_posts)
-
+    return render_template(
+        'index.html',
+        client_id=CLIENT_ID,
+        authenticated=Is_Authenticated(),
+        user_posts=user_posts
+    )
 
 
 if __name__ == '__main__':
     app.debug = True
-    app.run( 
-        host='0.0.0.0', 
+    app.run(
+        host='0.0.0.0',
         port=5000
     )
