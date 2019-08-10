cf59dec1bb3335c6881bd483c6c81d22db759f62
JustinTadlock <justin.b.tadlock@gmail.com>
2019-08-09 23:47:47 -0700

    Implementing Facebook Auth js
    Adding required functions to implement facebook login

--- static/scripts.js
+++ static/scripts.js
@@ -1,3 +1,4 @@
+
 window.onload = function() {
     $('.g-signin2 div div span span:last').text("Sign in with Google");
     $('.g-signin2 div div span span:first').text("Sign in with Google");
@@ -20,6 +21,60 @@ window.onload = function() {
     }
     xhr.send()
 }
+
+// This is called with the results from from FB.getLoginStatus().
+function statusChangeCallback(response) {
+    console.log('statusChangeCallback');
+    console.log(response);
+    // The response object is returned with a status field that lets the
+    // app know the current login status of the person.
+    // Full docs on the response object can be found in the documentation
+    // for FB.getLoginStatus().
+    if (response.status === 'connected') {
+        // Logged into your app and Facebook.
+        testAPI();
+    } else {
+        // The person is not logged into your app or we are unable to tell.
+        $('.result').html = 'Please log into this app.';
+    }
+}
+
+// This function is called when someone finishes with the Login
+// Button.  See the onlogin handler attached to it in the sample
+// code below.
+function checkLoginState() {
+    FB.getLoginStatus(function(response) {
+        statusChangeCallback(response);
+    });
+}
+
+window.fbAsyncInit = function() {
+    FB.init({
+        appId      : '434043257321789',
+        cookie     : true,  // enable cookies to allow the server to access 
+                            // the session
+        xfbml      : true,  // parse social plugins on this page
+        version    : 'v4.0' // The Graph API version to use for the call
+    });
+
+    // Now that we've initialized the JavaScript SDK, we call 
+    // FB.getLoginStatus().  This function gets the state of the
+    // person visiting this page and can return one of three states to
+    // the callback you provide.  They can be:
+    //
+    // 1. Logged into your app ('connected')
+    // 2. Logged into Facebook, but not your app ('not_authorized')
+    // 3. Not logged into Facebook and can't tell if they are logged into
+    //    your app or not.
+    //
+    // These three cases are handled in the callback function.
+
+    FB.getLoginStatus(function(response) {
+        statusChangeCallback(response);
+    });
+
+};
+
 function showSignInBtn(setVisible) {
     if(setVisible) {
         $('.sign-in').css('display', 'block');
