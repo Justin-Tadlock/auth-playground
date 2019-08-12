a9f9ba15ddeeee981adcbdc942957583b0110b5a
JustinTadlock <justin.b.tadlock@gmail.com>
2019-08-12 02:36:03 -0700

    Re-organize and add printLog

--- static/scripts.js
+++ static/scripts.js
@@ -1,8 +1,37 @@
+debug = false;
+
 window.onload = function() {
 
     checkAuthenticated();
 }
 
+
+window.fbAsyncInit = function() {
+    FB.init({
+        appId      : '434043257321789',
+        cookie     : true,  // enable cookies to allow the server to access 
+                            // the session
+        xfbml      : true,  // parse social plugins on this page
+        version    : 'v3.1' // The Graph API version to use for the call
+    });
+};
+
+
+// Load the SDK asynchronously
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+} (document, 'script', 'facebook-jssdk'));
+
+
+function printLog(message) {
+    if (debug) {
+        console.log(message);
+    }
+}
 
 function checkAuthenticated() {
     var xhr = new XMLHttpRequest();
@@ -11,13 +40,13 @@ function checkAuthenticated() {
     xhr.onload = function() {
         var response = JSON.parse(xhr.responseText);
 
-        console.log(response);
+        printLog(response);
         if (response.data) {
-            console.log("Logged in");
+            printLog("Logged in");
             showSignInBtn(false);
         }
         else {
-            console.log("Not logged in");
+            printLog("Not logged in");
             showSignInBtn(true);
         }
     }
@@ -25,20 +54,15 @@ function checkAuthenticated() {
 }
 
 
-function logout() {
-    console.log("Attempting logout...");
-    
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', '/logout');
-    xhr.setRequestHeader('content-type', 'application/x-www-form-urlencoded');
-    xhr.onload = function() {
-        console.log("Logging out the user");
-        $('.result').html("Successfully logged out! Redirecting in 2 seconds...");
-        setTimeout(function() {
-            window.location.href = '/';
-        }, 2000);
+function showSignInBtn(setVisible) {
+    if(setVisible) {
+        $('.google-auth, .facebook-auth').css('display', 'block');
+        $('.sign-out').css('display', 'none');
     }
-    xhr.send('logout=true');
+    else {
+        $('.google-auth, .facebook-auth').css('display', 'none');
+        $('.sign-out').css('display', 'block');
+    }
 }
 
 
@@ -48,34 +72,34 @@ function checkLoginState() {
     });
 }
 
+
 // This is called with the results from from FB.getLoginStatus().
 function statusChangeCallback(response) {
-    console.log('statusChangeCallback');
+    printLog('statusChangeCallback');
     
     // The response object is returned with a status field that lets the
     // app know the current login status of the person.
     // Full docs on the response object can be found in the documentation
     // for FB.getLoginStatus().
 
-    console.log('status: ', response.status);
+    printLog('status: ', response.status);
 
     if (response.status === 'connected') {
         // Logged into your app and Facebook.
         var access_token = FB.getAuthResponse()['accessToken'];
-        console.log('Successfully connected!');
+        printLog('Successfully connected!');
 
         var state = $('#state').data().state;
 
         FB.api('/me', function(response) {
-            console.log('Successful login for: ' + response.name);
+            printLog('Successful login for: ' + response.name);
 
+            // Login using Facebook credentials with our server
             var xhr = new XMLHttpRequest();
             xhr.open('POST', '/fbconnect');
             xhr.setRequestHeader('content-type', 'application/x-www-form-urlencoded');
             xhr.onload = function() {
-                console.log(xhr.responseText);
-
-                // Facebook verification response from server
+                // Verification response from server
                 var server_response = JSON.parse(xhr.responseText);
 
                 // If data is true, it means that the user is authenticated and needs to refresh page
@@ -92,41 +116,34 @@ function statusChangeCallback(response) {
 }
 
 
-window.fbAsyncInit = function() {
-    FB.init({
-        appId      : '434043257321789',
-        cookie     : true,  // enable cookies to allow the server to access 
-                            // the session
-        xfbml      : true,  // parse social plugins on this page
-        version    : 'v3.1' // The Graph API version to use for the call
-    });
-};
-
-// Load the SDK asynchronously
-(function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "https://connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-} (document, 'script', 'facebook-jssdk'));
 
 
-function showSignInBtn(setVisible) {
-    if(setVisible) {
-        $('.google-auth, .facebook-auth').css('display', 'block');
-        $('.sign-out').css('display', 'none');
+
+
+
+
+
+function logout() {
+    printLog("Attempting logout...");
+    
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', '/logout');
+    xhr.setRequestHeader('content-type', 'application/x-www-form-urlencoded');
+    xhr.onload = function() {
+        printLog("Logging out the user");
+        $('.result').html("Successfully logged out! Redirecting in 2 seconds...");
+        setTimeout(function() {
+            window.location.href = '/';
+        }, 2000);
     }
-    else {
-        $('.google-auth, .facebook-auth').css('display', 'none');
-        $('.sign-out').css('display', 'block');
-    }
+    xhr.send('logout=true');
 }
+
 
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
-        console.log('User signed out.');
+        printLog('User signed out.');
     });
 
     if (FB.getAccessToken() != null) {
@@ -139,7 +156,7 @@ function signOut() {
 }
 
 function onSignIn(googleUser) {
-    console.log("Enter onSignIn()"); 
+    printLog("Enter onSignIn()"); 
 
     var id_token = googleUser.getAuthResponse().id_token;
 
@@ -151,7 +168,7 @@ function onSignIn(googleUser) {
     xhr.onload = function() {
         var profile = googleUser.getBasicProfile();
 
-        console.log('Signed in as: ' + profile.getName());
+        printLog('Signed in as: ' + profile.getName());
 
         var server_response = JSON.parse(xhr.responseText);
 
