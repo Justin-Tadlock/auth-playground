44e350e726bf65550575f0c797dcb7ab236897b8
JustinTadlock <justin.b.tadlock@gmail.com>
2019-08-11 19:47:07 -0700

    Update App
    Adding generation of a state key for validation purposes
    
    Added best practice for try-except logic
    
    Added flask_cors for CSRF requests
    
    Added HTTPS functionality. Now the app runs from HTTPS instead of HTTP

--- start-app.py
+++ start-app.py
@@ -1,6 +1,9 @@
 import json
 import requests
 import httplib2
+import string
+import random
+import sys
 
 from flask import (
     Flask,
@@ -13,7 +16,7 @@ from flask import (
     render_template
 )
 
-import google_authentication as gAuth
+from flask_cors import CORS, cross_origin
 
 import google_authentication as gAuth
 
@@ -22,9 +25,23 @@ import google_authentication as gAuth
 app = Flask(__name__)
 try:
     app.secret_key = open('secret_key.txt', 'r').read()
-except:
+except IOError as ioe:
     print('Error: Please create a \'secret_key.txt\' file within the app\'s directory')
+    print(ioe.pgerror)
+    print(ioe.diag.message_detail)
+    sys.exit(1)
 
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+CORS(app, 
+     origins="http://localhost:5000", 
+     allow_headers=[
+        "Content-Type", 
+        "Authorization", 
+        "Access-Control-Allow-Credentials",
+        "Access-Control-Allow-Origin"
+    ],
+     supports_credentials=True)
 
 # Create data used to print into the user posts table
 user_posts = [
@@ -35,7 +52,14 @@ user_posts = [
 ]
 
 
+def Generate_State():
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(32))
+    login_session['state'] = state
+
+
 def Is_Authenticated():
+    print(login_session)
+
     return ('user' in login_session)
 
 
@@ -57,11 +81,15 @@ def Authenticated():
 
 @app.route('/')
 def Index():
+    print('Enter Index():')
+    Generate_State()
+
     return render_template(
         'index.html',
         client_id=gAuth.CLIENT_ID,
         authenticated=Is_Authenticated(),
-        user_posts=user_posts
+        user_posts=user_posts,
+        state=login_session['state']
     )
 
 
@@ -115,9 +143,10 @@ def G_Login():
     return make_response(jsonify(message="Already logged in", status=200, data="Already Logged In"))
 
 if __name__ == '__main__':
+    context = ('server.crt', 'server.key') #certificate and key files
     app.debug = True
     app.run(
-        ssl_context="adhoc",
+        ssl_context=context,
         host='0.0.0.0',
         port=5000
     )
